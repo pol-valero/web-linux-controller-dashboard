a43a5a9487319a6548f96ba893f353b70784c92e
Pol Valero <pol.valero.arjona@gmail.com>
2024-01-06 18:01:54 +0100

    Filter packages functionality almost done

---
+++ ASO F2/cgi-bin/edit_out_filter.sh
@@ -0,0 +1,104 @@
+#!/bin/bash
+
+table=$(echo "$QUERY_STRING" | awk -F'[=&?]' '{print $2}')
+action=$(echo "$QUERY_STRING" | awk -F'[=&?]' '{print $4}')
+
+echo -e "Content-type: text/html"
+
+echo -e "
+<!doctype html>
+<html>
+<head>
+    <link rel="stylesheet" href="../css/styles.css">
+    <form action="../cgi-bin/filter_pckgs.sh" method="get">
+        <button type="submit">Go back</button>
+    </form>
+</head>
+
+<body>
+
+    <h1>Edit $table filter rules</h1>
+"
+
+if [ $action = "ADD" ]; then
+
+    echo -e "
+        <form action='../cgi-bin/filter_pckgs.sh' method='get'>
+            <input type="hidden" id="table" name="table" value="$table">
+            <label>Target</label>
+            <select name='target'>
+                <option value='ACCEPT'>ACCEPT</option>
+                <option value='DROP'>DROP</option>
+                <option value='REJECT'>REJECT</option>
+            </select>
+            <br><br>
+            <label>Protocol</label>
+            <select name='protocol'>
+                <option value='tcp'>TCP</option>
+                <option value='udp'>UDP</option>
+                <option value='icmp'>ICMP</option>
+            </select>
+            <br><br>
+            <label>Source IP</label>
+            <input type='text' name='sourceip'>
+            <label>Destination IP</label>
+            <input type='text' name='destinationip'>
+            <br><br>
+            <label>Source port</label>
+            <input type='text' name='sourceport'>
+            <label>Destination port</label>
+            <input type='text' name='destinationport'>
+            <br><br>
+            <label>Interface</label>
+            <input type='text' name='interface'>
+            <br><br>
+            <button class='btn2' type='submit'>Add</button>
+    "
+
+else 
+
+    echo "<p>All the OUTPUT table rules have been deleted</p>"
+
+fi
+
+
+#TABLE-----------------------
+
+table_loaded=$(sudo iptables -L $table -n --line-numbers)
+
+echo -e "
+    <p><b>Current $table table</b></p>
+    <table border='1'>
+        <tr>
+            <th>Line</th><th>Target</th> <th>Prot</th><th>Opt</th><th>Source</th><th>Destination</th><th>Options</th>
+        </tr>
+    "
+
+while IFS= read -r line; do
+
+    #We omit the first to lines of the iptable output
+    if [ "$(echo "$line" | grep -c '^num')" -eq 0 ] && [ "$(echo "$line" | grep -c '^Chain')" -eq 0 ]; then
+
+        echo "<tr>"
+        for i in {1..7}; do
+            #For the last cell we put the last two columns of the output together
+            if [ "$i" -eq 7 ]; then
+                echo "<td>$(echo "$line" | awk '{print $7,$8}')</td>"
+            else 
+                echo "<td>$(echo "$line" | awk -v var="$i" '{print $var}')</td>"
+            fi
+        done
+        echo "</tr>"
+    fi
+done <<< "$table_loaded"
+
+echo "
+    </table>
+    "
+#------------------------------
+
+echo "
+
+</body>
+</html>
+"
---
+++ ASO F2/cgi-bin/filter_pckgs.sh
@@ -0,0 +1,113 @@
+#!/bin/bash
+
+
+#We get this values from the page where we add rules to the filter tables
+table=$(echo "$QUERY_STRING" | awk -F'[=&?]' '{print $2}')
+target=$(echo "$QUERY_STRING" | awk -F'[=&?]' '{print $4}')
+protocol=$(echo "$QUERY_STRING" | awk -F'[=&?]' '{print $6}')
+sourceip=$(echo "$QUERY_STRING" | awk -F'[=&?]' '{print $8}')
+destinationip=$(echo "$QUERY_STRING" | awk -F'[=&?]' '{print $10}')
+sourceport=$(echo "$QUERY_STRING" | awk -F'[=&?]' '{print $12}')
+destinationport=$(echo "$QUERY_STRING" | awk -F'[=&?]' '{print $14}')
+interface=$(echo "$QUERY_STRING" | awk -F'[=&?]' '{print $16}')
+
+command_arguments=""
+
+#We check if the user has entered a value for each field and we add it to the command arguments
+if [ -n "$sourceip" ]; then
+    command_arguments="-s $sourceip"
+fi
+
+if [ -n "$destinationip" ]; then
+    command_arguments="$command_arguments -d $destinationip"
+fi
+
+if [ -n "$interface" ]; then
+    command_arguments="$command_arguments -i $interface"
+fi
+
+if [ -n "$protocol" ]; then
+    command_arguments="$command_arguments -p $protocol"
+fi
+
+if [ -n "$sourceport" ]; then
+    command_arguments="$command_arguments --sport $sourceport"
+fi
+
+if [ -n "$destinationport" ]; then
+    command_arguments="$command_arguments --dport $destinationport"
+fi
+
+sudo iptables -A
+
+
+echo -e "Content-type: text/html"
+
+echo -e "
+<!doctype html>
+<html>
+<head>
+    <link rel="stylesheet" href="../css/styles.css">
+    <form action="../html/home.html" method="get">
+        <button type="submit">Home</button>
+    </form>
+</head>
+
+<body>
+
+    <h1>Filter Packages</h1>
+    
+"
+
+#OUTPUT TABLE-----------------------
+
+output_table=$(sudo iptables -L OUTPUT -n --line-numbers)
+
+echo -e "
+    <p><b>Current OUTPUT table</b></p>
+    <table border='1'>
+        <tr>
+            <th>Line</th><th>Target</th> <th>Prot</th><th>Opt</th><th>Source</th><th>Destination</th><th>Options</th>
+        </tr>
+    "
+
+while IFS= read -r line; do
+
+    #We omit the first to lines of the iptable output
+    if [ "$(echo "$line" | grep -c '^num')" -eq 0 ] && [ "$(echo "$line" | grep -c '^Chain')" -eq 0 ]; then
+
+        echo "<tr>"
+        for i in {1..7}; do
+            #For the last cell we put the last two columns of the output together
+            if [ "$i" -eq 7 ]; then
+                echo "<td>$(echo "$line" | awk '{print $7,$8}')</td>"
+            else 
+                echo "<td>$(echo "$line" | awk -v var="$i" '{print $var}')</td>"
+            fi
+        done
+        echo "</tr>"
+    fi
+done <<< "$output_table"
+
+echo "
+    </table>
+
+    <form action='../cgi-bin/edit_out_filter.sh' method='get'>
+        <button class="btn2">Add OUTPUT filter rules</button>
+        <input type="hidden" id="table" name="table" value="OUTPUT">
+        <input type="hidden" id="data" name="data" value="ADD">
+    </form>
+
+    <form action='../cgi-bin/edit_out_filter.sh' method='get'>
+        <button class="btn2">Delete OUTPUT filter rules</button>
+        <input type="hidden" id="table" name="table" value="OUTPUT">
+        <input type="hidden" id="data" name="data" value="DELETE">
+    </form>
+    "
+#-----------------------------------
+
+echo "
+
+</body>
+</html>
+"
